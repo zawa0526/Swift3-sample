4f235c99f5eb342d6a8bcb9975a00e2850d99dda
zawa0526 <ryo.t.0526@gmail.com>
2017-08-11 23:02:54 +0900

    first commit part-1

---
+++ part-1/part-1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  part-1
+//
+//  Created by takazawa on 2017/08/11.
+//  Copyright © 2017年 ryosuke takazawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
