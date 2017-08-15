364cf50bf6b0a4e25b4fe5c541ae6941386268e5
zawa0526 <ryo.t.0526@gmail.com>
2017-08-15 22:13:34 +0900

    Tabコントローラ 　バッジ表示も簡単

---
+++ 14-6-Tab/14-6-Tab/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  14-6-Tab
+//
+//  Created by takazawa on 2017/08/15.
+//  Copyright © 2017年 ryosuke takazawa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
