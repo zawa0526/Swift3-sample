b24bdd73bbf02b8bf8f3bbd32c4462f0b7c39ce9
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 18:51:42 +0900

    init 15

---
+++ 15-animation/15-animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  15-animation
+//
+//  Created by takazawa on 2017/08/16.
+//  Copyright © 2017年 ryosuke takazawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
