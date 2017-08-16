289375bf1c2d979dc4d1385148d7890c2c8fcbf7
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 21:44:18 +0900

    ビューをドラッグする

--- 16-GestureRecognizer/16-GestureRecognizer/SecondViewController.swift
+++ 16-GestureRecognizer/16-GestureRecognizer/SecondViewController.swift
@@ -12,7 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(hue: kakudo/360, saturation: 1.0, brightness: 1.0, alpha: 1.0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,32 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dragging(_ sender: UIPanGestureRecognizer) {
+        let tombo = sender.view!
+        tombo.center = sender.location(in: view)
+    }
 
+    // 0〜360 色相
+    var kakudo: CGFloat = 180.0
+    @IBAction func draggingOnView(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        // 水平方向の移動量
+        if translation.x > 0 {
+            kakudo -= 1.0
+        } else {
+            kakudo += 1.0
+        }
+        
+        // 0〜360の間で回す
+        if kakudo<0 {
+            kakudo += 360
+        } else if kakudo > 360{
+            kakudo -= 360
+        }
+                
+        let color = UIColor(hue: kakudo/360, saturation: 1.0, brightness: 1.0, alpha: 1.0)
+        view.backgroundColor = color
+    }
 }
 
