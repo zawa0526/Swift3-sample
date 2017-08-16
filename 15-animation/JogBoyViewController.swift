111493db9fe9627b12025bee5d36bed5f72541bd
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 20:29:15 +0900

    View作成

---
+++ 15-animation/JogBoyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  JogBoyViewController.swift
+//  15-animation
+//
+//  Created by takazawa on 2017/08/16.
+//  Copyright © 2017年 ryosuke takazawa. All rights reserved.
+//
+
+import UIKit
+
+class JogBoyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
