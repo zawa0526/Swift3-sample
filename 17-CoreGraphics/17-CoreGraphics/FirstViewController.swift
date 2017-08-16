27cfbc42474ced9694378d8015c61a4838bdb5d2
zawa0526 <ryo.t.0526@gmail.com>
2017-08-17 07:46:59 +0900

    図形イメージを描く

--- 17-CoreGraphics/17-CoreGraphics/FirstViewController.swift
+++ 17-CoreGraphics/17-CoreGraphics/FirstViewController.swift
@@ -9,10 +9,58 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    func makeBoxImage(width w: CGFloat, height h: CGFloat) -> UIImage {
+        
+        let size = CGSize(width: w, height: h)
+        // 開始
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        let drawRect = CGRect(x: 0, y: 0, width: w, height: h)
+        let drawPath = UIBezierPath(rect: drawRect)
+        
+        context?.setFillColor(red: 0.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        drawPath.fill()
+        context?.setStrokeColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        drawPath.stroke()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        // 終了
+        return image!
+    }
+    
+    func makeRoundRectImage(width w: CGFloat, height h: CGFloat, corner r: CGFloat) -> UIImage {
+        let size = CGSize(width: w, height: h)
+        // 開始
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        let drawRect = CGRect(x: 0, y: 0, width: w, height: h)
+        let drawPath = UIBezierPath(roundedRect: drawRect, cornerRadius: r)
+        
+        context?.setFillColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1.0)
+        drawPath.fill()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        // 終了
+        return image!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let boxImage = makeBoxImage(width: 240, height: 180)
+        let boxView = UIImageView(image: boxImage)
+        boxView.center = view.center
+        view.addSubview(boxView)
+        
+        let roundImage = makeRoundRectImage(width: 240, height: 180, corner: 20)
+        let roundView = UIImageView(image: roundImage)
+        roundView.center = view.center
+        view.addSubview(roundView)
     }
 
     override func didReceiveMemoryWarning() {
