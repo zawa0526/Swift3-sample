1455b83005c91542bb141707a122bb27c5ac1f44
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 20:50:02 +0900

    イメージビューのコマ送りアニメーション  スライダーと併せて

--- 15-animation/JogBoyViewController.swift
+++ 15-animation/JogBoyViewController.swift
@@ -9,6 +9,37 @@
 import UIKit
 
 class JogBoyViewController: UIViewController {
+    
+    @IBOutlet weak var boyView: UIImageView!
+    
+    @IBAction func runSpeed(_ sender: UISlider) {
+        print(sender.value)
+        // 再生秒数をスライダで決める
+        let sec = (1 - sender.value) * 3
+        // 再生にかかる秒数を設定
+        boyView.animationDuration = TimeInterval(sec)
+        
+        if sender.value == 0 {
+            boyView.stopAnimating()
+            boyView.image = UIImage(named: "jogboy_0")
+        } else {
+            if !boyView.isAnimating {
+                boyView.animationImages = jogboyImages()
+                boyView.animationRepeatCount = 0
+                boyView.startAnimating()
+            }
+        }
+    }
+    
+    func jogboyImages() -> Array<UIImage> {
+        var theArray = Array<UIImage>()
+        for num in 1...10 {
+            let imageName = "jogboy_" + String(num)
+            let image = UIImage(named: imageName)
+            theArray.append(image!)
+        }
+        return theArray
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
