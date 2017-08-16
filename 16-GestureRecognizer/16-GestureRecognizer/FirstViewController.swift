aa5de0bf8008ad98ccdc6727b19fec76fd3a21ee
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 21:09:09 +0900

    ImageViewをタップ

--- 16-GestureRecognizer/16-GestureRecognizer/FirstViewController.swift
+++ 16-GestureRecognizer/16-GestureRecognizer/FirstViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBAction func tapTombo(_ sender: UITapGestureRecognizer) {        
+        let tombo = sender.view!
+        
+        let newX = arc4random_uniform(UInt32(view.frame.width))
+        let newY = arc4random_uniform(UInt32(view.frame.height))
+        tombo.center = CGPoint(x: Double(newX), y: Double(newY))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
