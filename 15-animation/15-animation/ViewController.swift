78961ed89ead99259510f913254abed016bf070f
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 19:11:59 +0900

    タイマーを使ったアニメーション

--- 15-animation/15-animation/ViewController.swift
+++ 15-animation/15-animation/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var car: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // タイマーを作成
+        Timer.scheduledTimer(
+            timeInterval: 0.1,
+            target: self,
+            selector: #selector(self.step),
+            userInfo: nil,
+            repeats: true)
+        
+    }
+    
+    func step() {
+        car.center.x += 10
+        let carWitdth = car.bounds.width
+        if car.center.x > (view.bounds.width + carWitdth/2) {
+            car.center.x = -carWitdth
+            let viewH = view.bounds.height
+            car.center.y = CGFloat(arc4random_uniform(UInt32(viewH)))
+        }
     }
 
     override func didReceiveMemoryWarning() {
