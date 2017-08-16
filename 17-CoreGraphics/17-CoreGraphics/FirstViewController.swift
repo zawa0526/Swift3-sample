fb0eb06878b963e8f6d6a00a76a2a017d9d0389d
zawa0526 <ryo.t.0526@gmail.com>
2017-08-17 08:47:08 +0900

    扇形を描く  パーセントで表示する円弧グラフ

--- 17-CoreGraphics/17-CoreGraphics/FirstViewController.swift
+++ 17-CoreGraphics/17-CoreGraphics/FirstViewController.swift
@@ -82,6 +82,50 @@ class FirstViewController: UIViewController {
         UIGraphicsEndImageContext()
         return image!
     }
+    
+    func arcPercent(_ radius: CGFloat, _ percent: Double) -> UIBezierPath {
+        let endAngle = 2*Double.pi*percent/100 - Double.pi/2
+        let path = UIBezierPath(
+            arcCenter: CGPoint(x: 0, y: 0),  // 中心
+            radius: radius,  // 半径
+            startAngle: CGFloat(-Double.pi/2),  // 開始角度（0が3時)
+            endAngle: CGFloat(endAngle),  // 終了角度(π*5/3 = 300度)
+            clockwise: percent>0 // 時計回り
+        )
+        
+        return path
+    }
+    
+    func drawArcPercent() -> UIImage {
+        let size = view.bounds.size
+        // 開始
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        
+        let percent = 58.2
+        
+        UIColor.red.setStroke()
+        let arcPath = arcPercent(80, percent)
+        arcPath.lineWidth = 60
+        arcPath.lineCapStyle = .butt
+        
+        // パスを平行移動する
+        let tf = CGAffineTransform(translationX: view.center.x, y: view.center.y + 200)
+        arcPath.apply(tf)
+        arcPath.stroke()
+        
+        // 「何％」の数字を描く
+        let font = UIFont.boldSystemFont(ofSize: 28)
+        let textFontAttributes = [NSFontAttributeName: font,
+                                  NSForegroundColorAttributeName: UIColor.gray]
+        let drawString = String(percent) + "%"
+        let rect = CGRect(x: view.center.x - 45, y: view.center.y + 185, width: 90, height: 30)
+        drawString.draw(in: rect, withAttributes: textFontAttributes)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        // 終了
+        UIGraphicsEndImageContext()
+        return image!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -99,6 +143,10 @@ class FirstViewController: UIViewController {
         let lineImage = drawLine()
         let lineView = UIImageView(image: lineImage)
         view.addSubview(lineView)
+        
+        let arcImage = drawArcPercent()
+        let arcView = UIImageView(image: arcImage)
+        view.addSubview(arcView)
     }
 
     override func didReceiveMemoryWarning() {
