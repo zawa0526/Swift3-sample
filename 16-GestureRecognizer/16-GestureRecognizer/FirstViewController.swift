7eb393699fb9d4d9dac5142a908f0b4a7c026739
zawa0526 <ryo.t.0526@gmail.com>
2017-08-16 20:52:58 +0900

    init 16

---
+++ 16-GestureRecognizer/16-GestureRecognizer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  16-GestureRecognizer
+//
+//  Created by takazawa on 2017/08/16.
+//  Copyright © 2017年 ryosuke takazawa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
