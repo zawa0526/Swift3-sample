c7cffaa7670ec9f1a3cfdd4338315de0e3c5be9c
zawa0526 <ryo.t.0526@gmail.com>
2017-08-17 20:04:44 +0900

    折れ線グラフの描画

--- 17-CoreGraphics/17-CoreGraphics/SecondViewController.swift
+++ 17-CoreGraphics/17-CoreGraphics/SecondViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    func drawLine() -> UIImage {
+        var pointList = Array<CGPoint>()
+        let pointCount = 20
+        
+        let dx = Int(view.frame.width)/pointCount
+        let height = UInt32(view.frame.height)/2
+        for n in 1...pointCount {
+            let px = CGFloat(dx * n)
+            let py = CGFloat(arc4random_uniform(height) + 50)
+            let point = CGPoint(x: px, y: py)
+            pointList.append(point)
+        }
+        
+        let size = view.bounds.size
+        UIGraphicsBeginImageContextWithOptions(size, false, 1.0)
+        
+        let drawPath = UIBezierPath()
+        drawPath.move(to: pointList[0])
+        /// 以降はaddLineなので始点は削除
+        pointList.removeFirst()
+        
+        for pt in pointList {
+            drawPath.addLine(to: pt)
+        }
+        
+        // 線の属性
+        UIColor.blue.setStroke()
+        drawPath.lineWidth = 2.0
+        //  つなぎ目 .miter .round .bevel(http://cocoaapi.hatenablog.com/entry/00000108/recID34857)
+        drawPath.lineJoinStyle = .round
+        // 点線にする
+        drawPath.setLineDash([4.0, 2.0], count: 2, phase: 0.0)
+        drawPath.stroke()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let drawImage = drawLine()
+        let drawView = UIImageView(image: drawImage)
+        view.addSubview(drawView)
     }
 
     override func didReceiveMemoryWarning() {
