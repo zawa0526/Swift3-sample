1649ae2d8605caec4845576abb0aaca2c286ec02
zawa0526 <ryo.t.0526@gmail.com>
2017-08-17 20:55:09 +0900

    init 18

---
+++ 18-SaveData/18-SaveData/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  18-SaveData
+//
+//  Created by takazawa on 2017/08/17.
+//  Copyright © 2017年 ryosuke takazawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
