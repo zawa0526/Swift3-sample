e86564ca0c773dd810258e538a10933148eaeeaf
zawa0526 <ryo.t.0526@gmail.com>
2017-08-17 21:43:00 +0900

    Float値の保存・読込  UISliderとUIProgress

--- 18-SaveData/18-SaveData/FirstViewController.swift
+++ 18-SaveData/18-SaveData/FirstViewController.swift
@@ -14,6 +14,10 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var mySwitch1: UISwitch!
     @IBOutlet weak var mySwitch2: UISwitch!
     
+    @IBOutlet weak var mySlider1: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var myProgress: UIProgressView!
+    @IBOutlet weak var progressLabel: UILabel!
     
     @IBAction func tapSave(_ sender: UIButton) {
         let defaults = UserDefaults.standard
@@ -25,11 +29,29 @@ class FirstViewController: UIViewController {
         mySwitch2.isOn = defaults.bool(forKey: "switchOn")
     }
     
+    @IBAction func tapSliderSave(_ sender: UIButton) {
+        let defaults = UserDefaults.standard
+        let value = mySlider1.value
+        
+        defaults.set(value, forKey: "sliderValue")
+    }
+    
+    @IBAction func tapSliderRead(_ sender: UIButton) {
+        let defaults = UserDefaults.standard
+        let value = defaults.float(forKey: "sliderValue")
+        
+        progressLabel.text = String(value)
+        myProgress.progress = value
+    }
+    
+    @IBAction func slide(_ sender: UISlider) {
+        sliderLabel.text = String(sender.value)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
